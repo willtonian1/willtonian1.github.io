1d74815007d25f10598d6edb6fd22bf19119153e
willtonian1 <williamstom2005@gmail.com>
2022-04-18 18:24:40 +0100

    new Plane design
    working THREE scene with HTML text and plane

--- Cube/script.js
+++ Cube/script.js
@@ -1,61 +1,30 @@
 // The three.js scene: the 3D world where you put objects
+import { OrbitControls } from 'https://cdn.skypack.dev/three@0.136.0/examples/jsm/controls/OrbitControls.js';
+
 const scene = new THREE.Scene();
 
 // The camera
 const camera = new THREE.PerspectiveCamera(
-    60,
+    75,
     window.innerWidth / window.innerHeight,
     1,
-    10000
+    1000
 );
 
+
 // The renderer: something that draws 3D objects onto the canvas
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+
+renderer.setSize(innerWidth, innerHeight)
 
 // Append the renderer canvas into <body>
-const canvas = document.querySelector('#c')
-const renderer = new THREE.WebGLRenderer({ canvas });
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(0xaaaaaa, 1);
+document.body.appendChild(renderer.domElement);
 
+const light = new THREE.DirectionalLight(0xFFFFFF, 1, )
 
+light.position.set(0, 0, 1)
+scene.add(light)
 
-// A cube we are going to animate
-const cube = {
-    // The geometry: the shape & size of the object
-    geometry: new THREE.BoxGeometry(1, 1, 1),
-    // The material: the appearance (color, texture) of the object
-    material: new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-};
+camera.position.z = 5
 
-const cube2 = {
-    geometry: new THREE.BoxGeometry(2, 2, 2),
-    material: new THREE.MeshBasicMaterial({ color: 0xff0000 })
-};
-
-
-
-// The mesh: the geometry and material combined, and something we can directly add into the scene (I had to put this line outside of the object literal, so that I could use the geometry and material properties)
-cube.mesh = new THREE.Mesh(cube.geometry, cube.material);
-
-cube2.mesh = new THREE.Mesh(cube2.geometry, cube2.material);
-
-// Add the cube into the scene
-scene.add(cube.mesh);
-//scene.add(cube2.mesh);
-scene.background = new THREE.Color('yellow');
-// Make the camera further from the cube so we can see it better
-camera.position.z = 5;
-
-function render() {
-    // Render the scene and the camera
-    renderer.render(scene, camera);
-
-    // Rotate the cube every frame
-    cube.mesh.rotation.x += 0.05;
-    cube.mesh.rotation.y -= 0.05;
-
-    // Make it call the render() function about every 1/60 second
-    requestAnimationFrame(render);
-}
-
-render();
+//plane time
