e21503525496453281b5fccc2dea35864b2d517f
Tom Williams <77905483+willtonian1@users.noreply.github.com>
2022-04-18 18:27:40 +0100

    forgot some code -- DONE

--- Cube/script.js
+++ Cube/script.js
@@ -5,10 +5,10 @@ const scene = new THREE.Scene();
 
 // The camera
 const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    1,
-    1000
+  75,
+  window.innerWidth / window.innerHeight,
+  1,
+  1000
 );
 
 
@@ -22,9 +22,30 @@ document.body.appendChild(renderer.domElement);
 
 const light = new THREE.DirectionalLight(0xFFFFFF, 1, )
 
-light.position.set(0, 0, 1)
+light.position.set(0,0,1)
 scene.add(light)
 
 camera.position.z = 5
 
 //plane time
+
+const planeGeo = new THREE.PlaneGeometry(8,8,10,10)
+const planeMat = new THREE.MeshPhongMaterial({
+	color: 0xff00000,
+	side: THREE.DoubleSide
+})
+const PlaneMesh = new THREE.Mesh(planeGeo, planeMat)
+scene.add(PlaneMesh)
+
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.screenSpacePanning = true;
+
+
+function animate(){
+	requestAnimationFrame(animate)
+	renderer.render(scene, camera)
+	  controls.update();
+}
+
+animate()
