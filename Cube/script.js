3d7bc4652bc0f694f8b2db843d3eddb3b7a8e58d
willtonian1 <williamstom2005@gmail.com>
2022-04-18 15:20:49 +0100

    trying things
    testing

--- Cube/script.js
+++ Cube/script.js
@@ -3,31 +3,33 @@ const scene = new THREE.Scene();
 
 // The camera
 const camera = new THREE.PerspectiveCamera(
-  60,
-  window.innerWidth / window.innerHeight,
-  1,
-  10000
+    60,
+    window.innerWidth / window.innerHeight,
+    1,
+    10000
 );
 
 // The renderer: something that draws 3D objects onto the canvas
-const renderer = new THREE.WebGLRenderer({ antialias: true });
+
+// Append the renderer canvas into <body>
+const canvas = document.querySelector('#c')
+const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(0xaaaaaa, 1);
-// Append the renderer canvas into <body>
-document.body.appendChild(renderer.domElement);
+
 
 
 // A cube we are going to animate
 const cube = {
-  // The geometry: the shape & size of the object
-  geometry: new THREE.BoxGeometry(1, 1, 1),
-  // The material: the appearance (color, texture) of the object
-  material: new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+    // The geometry: the shape & size of the object
+    geometry: new THREE.BoxGeometry(1, 1, 1),
+    // The material: the appearance (color, texture) of the object
+    material: new THREE.MeshBasicMaterial({ color: 0x00ff00 })
 };
 
 const cube2 = {
-	geometry: new THREE.BoxGeometry(2,2,2), 
-	material: new THREE.MeshBasicMaterial({ color: 0xff0000 })
+    geometry: new THREE.BoxGeometry(2, 2, 2),
+    material: new THREE.MeshBasicMaterial({ color: 0xff0000 })
 };
 
 
@@ -39,21 +41,21 @@ cube2.mesh = new THREE.Mesh(cube2.geometry, cube2.material);
 
 // Add the cube into the scene
 scene.add(cube.mesh);
-scene.add(cube2.mesh);
-
+//scene.add(cube2.mesh);
+scene.background = new THREE.Color('yellow');
 // Make the camera further from the cube so we can see it better
 camera.position.z = 5;
 
 function render() {
-  // Render the scene and the camera
-  renderer.render(scene, camera);
+    // Render the scene and the camera
+    renderer.render(scene, camera);
 
-  // Rotate the cube every frame
-  cube.mesh.rotation.x += 0.05;
-  cube.mesh.rotation.y -= 0.05;
+    // Rotate the cube every frame
+    cube.mesh.rotation.x += 0.05;
+    cube.mesh.rotation.y -= 0.05;
 
-  // Make it call the render() function about every 1/60 second
-  requestAnimationFrame(render);
+    // Make it call the render() function about every 1/60 second
+    requestAnimationFrame(render);
 }
 
 render();
