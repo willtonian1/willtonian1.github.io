0d014b36182aac4f16575b482b9327cbcfa413c9
willtonian1 <williamstom2005@gmail.com>
2022-04-19 17:36:56 +0100

    new format and mapping three.js
    tube map texture, and columns

--- Cube/script.js
+++ Cube/script.js
@@ -6,25 +6,57 @@ const scene = new THREE.Scene();
 // The camera
 const camera = new THREE.PerspectiveCamera(
     75,
-    window.innerWidth / window.innerHeight,
+    (window.innerWidth / 2) / window.innerHeight,
     1,
     1000
 );
 
 
 // The renderer: something that draws 3D objects onto the canvas
-const renderer = new THREE.WebGLRenderer({ antialias: true });
+const renderer = new THREE.WebGLRenderer({
+    antialias: true,
+    canvas: document.querySelector('canvas')
+});
 
-renderer.setSize(innerWidth, innerHeight)
+renderer.setSize(innerWidth / 2, innerHeight)
 
 // Append the renderer canvas into <body>
-document.body.appendChild(renderer.domElement);
+//document.body.appendChild(renderer.domElement);
 
 const light = new THREE.DirectionalLight(0xFFFFFF, 1, )
 
 light.position.set(0, 0, 1)
 scene.add(light)
 
-camera.position.z = 5
+camera.position.z = 10
 
 //plane time
+
+var textureLoader = new THREE.TextureLoader();
+const planeTexture = new textureLoader.load("tube.jpg")
+
+
+
+const planeGeo = new THREE.PlaneGeometry(7.2, 7.2, 10, 10)
+const planeMat = new THREE.MeshPhongMaterial({
+    color: 0xffffff,
+    side: THREE.DoubleSide,
+    map: planeTexture
+})
+const PlaneMesh = new THREE.Mesh(planeGeo, planeMat)
+scene.add(PlaneMesh)
+
+
+
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.screenSpacePanning = true;
+
+
+function animate() {
+    requestAnimationFrame(animate)
+    renderer.render(scene, camera)
+    controls.update();
+}
+
+animate()
