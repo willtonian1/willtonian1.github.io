df03487111b82f41ccc9ed6d02a31b3552afdccc
willtonian1 <williamstom2005@gmail.com>
2022-05-11 19:49:24 +0100

    fixed scrolling, and sizing of renderer canvas

--- script.js
+++ script.js
@@ -1,81 +1,68 @@
+"use strict";
+
+
 // The three.js scene: the 3D world where you put objects
 import { OrbitControls } from 'https://cdn.skypack.dev/three@0.136.0/examples/jsm/controls/OrbitControls.js';
 
+
+
+
+const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector("canvas") });
+
+// There's no reason to set the aspect here because we're going
+// to set it every frame anyway so we'll set it to 2 since 2
+// is the the aspect for the canvas default size (300w/150h = 2)
+const camera = new THREE.PerspectiveCamera(70, 2, 1, 1000);
+camera.position.z = 400;
+
 const scene = new THREE.Scene();
-
-// The camera
-const camera = new THREE.PerspectiveCamera(
-    75,
-    2,
-    0.1,
-    5
-);
-
-
-// The renderer: something that draws 3D objects onto the canvas
-const renderer = new THREE.WebGLRenderer({
-    antialias: true,
-    canvas: document.querySelector('#c')
+const geometry = new THREE.BoxGeometry(200, 200, 200);
+const material = new THREE.MeshPhongMaterial({
+    color: 0x555555,
+    specular: 0xffffff,
+    shininess: 50,
+    shading: THREE.SmoothShading
 });
-
-renderer.setSize(innerWidth/2, innerHeight)
-
-// Append the renderer canvas into <body>
-//document.body.appendChild(renderer.domElement);
-
-
-
-
-const color = 0xFFFFFF;
-const intensity = 0.5;
-const light2 = new THREE.AmbientLight(color, intensity);
-scene.add(light2);
-
-camera.position.z = 4
-
 
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.screenSpacePanning = true;
 
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
+const light1 = new THREE.PointLight(0xff80C0, 2, 0);
+light1.position.set(200, 100, 300);
+scene.add(light1);
 
+function resizeCanvasToDisplaySize() {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    if (canvas.width !== width || canvas.height !== height) {
+        // you must pass false here or three.js sadly fights the browser
+        renderer.setSize(width, height, false);
+        camera.aspect = width / height;
+        camera.updateProjectionMatrix();
 
+        // set render target sizes here
+        controls.update();
+    }
+}
 
-
-
-
-//texture
-
-var textureLoader2 = new THREE.TextureLoader();
-const planeTexture2 = new textureLoader2.load("assets/ocean.jpg")
-
-//sphere time 
-
-const geometry = new THREE.SphereGeometry(0.1, 32, 16);
-const material = new THREE.MeshBasicMaterial({ color: 0xffffff, map: planeTexture2 });
-const sphere = new THREE.Mesh(geometry, material);
-scene.add(sphere);
-sphere.position.set(0, 0, 3)
 
 function animate(time) {
-    sphere.rotation.y += 0.05;
-    
+    time *= 0.001; // seconds
+
+    resizeCanvasToDisplaySize();
+
+    mesh.rotation.x = time * 0.5;
+    mesh.rotation.y = time * 1;
+
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+
     controls.update();
-    time *=0.001;
-    const canvas = renderer.domElement;
-    camera.aspect = canvas.clientWidth / canvas.clientHeight;
-    camera.updateProjectionMatrix();
+}
 
-    
-    renderer.render(scene, camera)
-    requestAnimationFrame(animate)
-
-  }
-
-
-
-animate()
-
-
-
+requestAnimationFrame(animate);
