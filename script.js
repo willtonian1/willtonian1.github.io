3f4d6497b63000ab3aa38f7d072355d996390d33
willtonian1 <williamstom2005@gmail.com>
2022-05-21 20:26:14 +0100

    new meshes, with video texture

--- script.js
+++ script.js
@@ -16,16 +16,40 @@ const camera = new THREE.PerspectiveCamera(70, 2, 1, 1000);
 camera.position.z = 400;
 
 const scene = new THREE.Scene();
-const geometry = new THREE.BoxGeometry(200, 200, 200);
-const material = new THREE.MeshPhongMaterial({
-    color: 0x0000ff,
-    specular: 0xff0000,
-    shininess: 30,
-    shading: THREE.SmoothShading
+
+
+
+
+let video = document.getElementById('video');
+let texture2 = new THREE.VideoTexture(video);
+texture2.needsUpdate;
+texture2.minFilter = THREE.LinearFilter;
+texture2.magFilter = THREE.LinearFilter;
+
+video.src = "assets/get-hyper.mp4";
+video.load();
+video.play();
+
+
+
+const geometry = new THREE.PlaneGeometry(200, 200);
+const material = new THREE.MeshBasicMaterial({
+
+    shading: THREE.SmoothShading,
+    map: texture2,
+    side: THREE.DoubleSide,
+    color: 0xffffff
+
+
 });
 
-const light = new THREE.AmbientLight(0xa0a0a0); // soft white light
+const light = new THREE.AmbientLight(0xffffff); // soft white light
 scene.add(light);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+scene.add(directionalLight);
+
+directionalLight.position.set(0, 5, 10);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.screenSpacePanning = true;
@@ -33,9 +57,94 @@ controls.screenSpacePanning = true;
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
-const light1 = new THREE.PointLight(0xff80C0, 2, 0);
-light1.position.set(200, 100, 300);
-scene.add(light1);
+
+
+
+const geometry2 = new THREE.BoxGeometry(220, 10, 10);
+const material2 = new THREE.MeshBasicMaterial({ color: 0xffffff });
+const mesh2 = new THREE.Mesh(geometry2, material2);
+scene.add(mesh2);
+mesh2.position.set(0, 105, 0);
+
+
+
+const geometry3 = new THREE.BoxGeometry(220, 10, 10);
+const material3 = new THREE.MeshBasicMaterial({ color: 0xffffff });
+const mesh3 = new THREE.Mesh(geometry3, material3);
+scene.add(mesh3);
+mesh3.position.set(105, 0, 0);
+mesh3.rotation.z = 1.5708;
+
+const geometry4 = new THREE.BoxGeometry(220, 10, 10);
+const material4 = new THREE.MeshBasicMaterial({ color: 0xffffff });
+const mesh4 = new THREE.Mesh(geometry4, material4);
+scene.add(mesh4);
+mesh4.position.set(-105, 0, 0);
+mesh4.rotation.z = 1.5708;
+
+
+
+const geometry5 = new THREE.BoxGeometry(220, 10, 10);
+const material5 = new THREE.MeshBasicMaterial({ color: 0xffffff });
+const mesh5 = new THREE.Mesh(geometry5, material5);
+scene.add(mesh5);
+mesh5.position.set(0, -105, 0);
+
+
+
+
+const geometry6 = new THREE.BoxGeometry(50, 10, 10);
+const material6 = new THREE.MeshBasicMaterial({ color: 0xffffff });
+const mesh6 = new THREE.Mesh(geometry6, material6);
+scene.add(mesh6);
+mesh6.position.set(-17, -125, 0);
+mesh6.rotation.z = 1.5708 / 2;
+
+
+
+
+const geometry7 = new THREE.BoxGeometry(50, 10, 10);
+const material7 = new THREE.MeshBasicMaterial({ color: 0xffffff });
+const mesh7 = new THREE.Mesh(geometry7, material7);
+scene.add(mesh7);
+mesh7.position.set(17, -125, 0);
+mesh7.rotation.z = -1.5708 / 2;
+
+
+
+
+
+const geometry8 = new THREE.BoxGeometry(30, 70, 30);
+const material8 = new THREE.MeshBasicMaterial({ color: 0xffffff });
+const mesh8 = new THREE.Mesh(geometry8, material8);
+scene.add(mesh8);
+mesh8.position.set(145, -110, 5);
+
+
+const geometry9 = new THREE.SphereGeometry(15, 32, 16);
+const material9 = new THREE.MeshBasicMaterial({ color: 0x000000 });
+const sphere = new THREE.Mesh(geometry9, material9);
+scene.add(sphere);
+
+sphere.position.set(145, -100, 10)
+
+
+
+
+const geometry10 = new THREE.SphereGeometry(15, 32, 16);
+const material10 = new THREE.MeshBasicMaterial({ color: 0x000000 });
+const sphere2 = new THREE.Mesh(geometry10, material10);
+scene.add(sphere2);
+
+sphere2.position.set(145, -125, 10)
+
+
+
+
+
+
+
+
 
 function resizeCanvasToDisplaySize() {
     const canvas = renderer.domElement;
@@ -58,8 +167,10 @@ function animate(time) {
 
     resizeCanvasToDisplaySize();
 
-    mesh.rotation.x = time * 0.5;
-    mesh.rotation.y = time * 1;
+    //mesh.rotation.x = time * 0.5;
+    //mesh.rotation.y = time * 1;
+
+    texture2.needsUpdate = true;
 
     renderer.render(scene, camera);
     requestAnimationFrame(animate);
